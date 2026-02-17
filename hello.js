df06ab4ab5b1d812da9835cd86ff8d9a7551298c
Shlok Rastogi <shlokrastogi2004@gmail.com>
2026-02-18 04:06:35 +0530

    install node.js understand how module exports works

---
+++ hello.js
@@ -0,0 +1,6 @@
+const math = require("./math");
+
+console.log("hey, I am JS");
+console.log(math);
+console.log(math.add2Numbers(2, 3));
+console.log(math.sub2Numbers(2, 3));
---
+++ math.js
@@ -0,0 +1,11 @@
+// function add2Numbers(a, b) {
+//   return a + b;
+// }
+// function sub2Numbers(a, b) {
+//   return a - b;
+// }
+
+// module.exports = { add2Numbers, sub2Numbers };
+
+exports.add2Numbers = (a, b) => a + b;
+exports.sub2Numbers = (a, b) => a - b;
