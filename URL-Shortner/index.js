c2f840e7d2b04a699344f045b681fa16029143e2
Shlok Rastogi <shlokrastogi2004@gmail.com>
2026-03-15 21:30:21 +0530

    create the frontend from Server Side Rendering

--- URL-Shortner/index.js
+++ URL-Shortner/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const path = require("path");
 const URL = require("./models/url");
+const staticRoute = require("./routes/staticRouter");
 
 const app = express();
 const PORT = 4001;
@@ -10,12 +12,19 @@ connectToMongoDB("mongodb://localhost:27017/url-shortener").then(() => {
   console.log("Connected to MongoDB");
 });
 
-app.use(express.json());
-app.use("/url", urlRoute);
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
-app.get("/", (req, res) => {
-  return res.send("Hello World");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/test", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home", { urls: allUrls });
 });
+
+app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
 app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
